f03ebaf87baf41ec0bc950bf27c39922cb396542
puneetcse09 <puneetcse09@gmail.com>
2015-11-02 20:28:27 +0530

    fixes

--- WebContent/properties/sqls/AMSS-Profile-Updates.sql
+++
@@ -1,4 +0,0 @@
-select INITIATOR_NAME, request_status, utl_raw.cast_to_varchar2(REQUEST_CONTENT)
-  from PSSDBO1.request
- where request_type = 'CUST'
-   and substr(LAST_UPDATE_DATE, 0, 10) = to_char(SYSDATE, 'YYYY-MM-DD')
--- WebContent/properties/sqls/the10b-PendingItems.sql
+++
@@ -1,2 +0,0 @@
-/* TODO: write this. The file was empty in CVS... */
-select SYSDATE from DUAL
